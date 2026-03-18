f601a150d44afc7453f6eec4b55d3e1441d529b2
SK <164775232+klymsk@users.noreply.github.com>
2026-03-18 11:45:47 +0200

    fix

---
+++ validator/src/main.py
@@ -0,0 +1,29 @@
+from parser.schema_parser import SchemaParser
+from parser.data_parser import DataParser
+from validator.validator import Validator
+from report.report import ReportGenerator
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))
+
+def main():
+    with open("../schema.def") as f:
+        schema_text = f.read()
+
+    with open("../data.txt") as f:
+        data_text = f.read()
+
+    schemas = SchemaParser().parse(schema_text)
+    data = DataParser().parse(data_text)
+
+    schema = schemas[data.type_name]
+
+    result = Validator().validate(schema, data)
+
+    ReportGenerator().generate(result)
+
+
+if __name__ == "__main__":
+    main()
