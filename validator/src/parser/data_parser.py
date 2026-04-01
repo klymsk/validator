fbe9543c812d35acb02960470bef5ba855aef7ae
SK <164775232+klymsk@users.noreply.github.com>
2026-04-01 10:20:39 +0300

    refactor: data parser module

--- validator/src/parser/data_parser.py
+++ validator/src/parser/data_parser.py
@@ -1,18 +1,31 @@
-import json
-from exceptions.custom_exceptions import ParserError
+import re
 
 class DataParser:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def parse(self, text):
+        text = re.sub(r'(\w+):', r'"\1":', text)
+        text = text.replace("true", "True").replace("false", "False")
 
-    def parse(self):
-        try:
-            with open(self.file_path, "r", encoding="utf-8") as f:
-                return json.load(f)
+        # прибираємо типи типу User {
+        text = re.sub(r'\w+\s*{', '{', text)
 
-        except FileNotFoundError:
-            raise ParserError("Файл не знайдено")
+        return eval(text)
 
-        except json.JSONDecodeError:
-            raise ParserError("Невірний формат JSON")
+
+# import json
+# from exceptions.custom_exceptions import ParserError
+
+# class DataParser:
+#     def __init__(self, file_path):
+#         self.file_path = file_path
+
+#     def parse(self):
+#         try:
+#             with open(self.file_path, "r", encoding="utf-8") as f:
+#                 return json.load(f)
+
+#         except FileNotFoundError:
+#             raise ParserError("Файл не знайдено")
+
+#         except json.JSONDecodeError:
+#             raise ParserError("Невірний формат JSON")
         
